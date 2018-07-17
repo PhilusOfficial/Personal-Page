bc198afb113bbb46b35415275e7c02340cfcd787
PhilusOfficial <luciricofedefora@gmail.com>
2018-07-18 00:08:37 +0200

    1

--- projectsscript.js
+++ projectsscript.js
@@ -13,8 +13,6 @@ function goback() {
 $(".video").click(scale)
 $("#cover").click(goback)
 
-
-
 function onReady(callback) {
   var intervalId = window.setInterval(function() {
     if (document.getElementsByTagName('body')[0] !== undefined) {
