bf047f1190a45e6017f3f8f797bd287aaafc52e1
PhilusOfficial <luciricofedefora@gmail.com>
2018-07-17 23:50:30 +0200

    1

---
+++ projectsscript.js
@@ -0,0 +1,34 @@
+function scale() {
+  $(this).addClass("center")
+  $("#cover").addClass("depth")
+  $("body").addClass("stopscroll")
+}
+
+function goback() {
+  $(".video").removeClass("center")
+  $("#cover").removeClass("depth")
+  $("body").removeClass("stopscroll")
+}
+
+$(".video").click(scale)
+$("#cover").click(goback)
+
+
+
+function onReady(callback) {
+  var intervalId = window.setInterval(function() {
+    if (document.getElementsByTagName('body')[0] !== undefined) {
+      window.clearInterval(intervalId);
+      callback.call(this);
+    }
+  }, 1000);
+}
+
+function setVisible(selector, visible) {
+  document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
+
+onReady(function() {
+  setVisible('.page', true);
+  setVisible('#loading', false);
+});
